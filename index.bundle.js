7ac9216d5e941dfe2ec491bf2639df8b86ead6b9
zhna123 <cszhna@gmail.com>
2022-02-19 15:29:59 -0500

    project setup

---
+++ index.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("set up project successfully - edit!");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsbUQiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvX2xpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJzZXQgdXAgcHJvamVjdCBzdWNjZXNzZnVsbHkgLSBlZGl0IVwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
