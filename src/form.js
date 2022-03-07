ce72d5e2bf7f2923c80e7f033625fd720985c083
zhna123 <cszhna@gmail.com>
2022-03-06 22:01:20 -0500

    wip - add todo form

--- src/form.js
+++ src/form.js
@@ -1,7 +1,7 @@
 import Close_Icon from './images/close_form.svg';
 import ExpandMore_Icon from './images/expand_more.svg';
 import ExpandLess_Icon from './images/expand_less.svg';
-import {createNewProject, saveTodoItemInProject} from './project.js';
+import {createNewProject, saveTodoItemInProject, getAllProjects} from './project.js';
 import {TodoItem} from './todoItem.js';
 
 export function openProjectForm() {
@@ -111,6 +111,71 @@ function todoForm(project, projectBtn) {
     form.appendChild(moreOptionDiv);
 
     // more fields - hidden by default
+    const hiddenForm = document.createElement("div");
+    hiddenForm.classList.add("hidden");
+    // project drop down
+    const selectProjDiv = document.createElement("div");
+    selectProjDiv.classList.add("select_div");
+    const selectProject = document.createElement("select");
+
+    const option = document.createElement("option");
+    option.value = "";
+    option.disabled = true;
+    option.selected = true;
+    option.textContent = "Select project";
+    selectProject.appendChild(option);
+
+    const projects = getAllProjects();
+    projects.forEach(p => {
+        const option = document.createElement("option");
+        option.value = p.val.name;
+        option.textContent = p.val.name;
+        selectProject.appendChild(option);
+    })
+
+    selectProjDiv.appendChild(selectProject);
+    form.appendChild(selectProjDiv);
+
+    // priority drop down
+    const selectPriorityDiv = document.createElement("div");
+    selectPriorityDiv.classList.add("select_div");
+    const selectPriority = document.createElement("select");
+
+    const priorityOption = document.createElement("option");
+    priorityOption.value = "";
+    priorityOption.disabled = true;
+    priorityOption.selected = true;
+    priorityOption.textContent = "Select priority";
+    selectPriority.appendChild(priorityOption);
+
+    const allPriorities = ["Low", "Medium", "High"]
+    allPriorities.forEach((p, index) => {
+        const option = document.createElement("option");
+        option.value = p;
+        option.textContent = p;
+        selectPriority.appendChild(option);
+    })
+
+    selectPriorityDiv.appendChild(selectPriority);
+    form.appendChild(selectPriorityDiv);
+
+    // due date
+    const dueDateDiv = document.createElement("div");
+    dueDateDiv.classList.add("dueDate_div");
+    const dateInput = document.createElement("input");
+    dateInput.type = "date";
+    dateInput.name = "dueDate";
+    dateInput.placeholder = "Due date"
+    dueDateDiv.appendChild(dateInput);
+    form.appendChild(dueDateDiv);
+
+    // textarea
+    const textareaDiv = document.createElement("div");
+    textareaDiv.classList.add("textarea_div");
+    const textarea = document.createElement("textarea");
+    textarea.placeholder = "Add description …";
+    textareaDiv.appendChild(textarea);
+    form.appendChild(textareaDiv);
 
     const btnDiv = document.createElement("div")
     btnDiv.classList.add("btn_div");
